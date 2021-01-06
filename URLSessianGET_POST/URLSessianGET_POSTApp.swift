116f74992cd564cd2d53265361adab5cf31531e3
DevDataOrg <48391348+DevDataOrg@users.noreply.github.com>
2021-01-06 19:54:32 +0300

    Initial Commit

---
+++ URLSessianGET_POST/URLSessianGET_POSTApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessianGET_POSTApp.swift
+//  URLSessianGET_POST
+//
+//  Created by Artyom Romanchuk on 06.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct URLSessianGET_POSTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
